19ba8e5182e895d427aab12b20a82c3d4d391b03
Thai Dang <thai.dang.fr@gmail.com>
2017-04-13 18:05:01 +0200

    fix rspec bug

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
+include Capybara::RSpecMatchers
 
 RSpec.describe PagesController, type: :controller do
+	render_views
 
   describe "GET #home" do
     it "returns http success" do
@@ -8,11 +10,10 @@ RSpec.describe PagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-#    it "devrait avoir le bon titre" do
-#      get :home
-#      expect(response.body).to have_title('Simple app du tutoriel ruby on rails | accueil')
-#    end
-
+    it "devrait avoir le bon titre" do
+      get :home
+      expect(response.body).to have_selector('title', text: 'Simple App du Tutoriel Ruby on Rails | Accueil', visible: false)
+    end
   end
 
   describe "GET #contact" do
@@ -21,10 +22,10 @@ RSpec.describe PagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-#    it "devrait avoir le bon titre" do
-#      get :contact
-#      expect(response.body).to have_title('Simple app du tutoriel ruby on rails | contact')
-#    end
+    it "devrait avoir le bon titre" do
+      get :contact
+      expect(response.body).to have_selector('title', text: 'Simple App du Tutoriel Ruby on Rails | Contact', visible:false)
+    end
   end
 
   describe "GET #about" do
@@ -33,10 +34,10 @@ RSpec.describe PagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-#    it "devrait avoir le bon titre" do
-#      get :about
-#      expect(response.body).to have_title('Simple app du tutoriel ruby on rails | a propos')
-#    end
+    it "devrait avoir le bon titre" do
+      get :about
+      expect(response.body).to have_selector('title', text: 'Simple App du Tutoriel Ruby on Rails | A propos', visible: false)
+    end
   end
 
   describe "GET #help" do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
+include Capybara::RSpecMatchers
 
 RSpec.describe UsersController, type: :controller do
+	render_views
 
+	describe "get 'show'" do
+		before(:each) do
+			@user = FactoryGirl.create(:user)
+		end
 
-describe "get 'show'" do
-before(:each) do
-@user = Factory(:user)
-end
+		it "devrait reussir" do
+			get :show, params: {id: @user}
+			expect(response).to have_http_status(:success)
+		end
 
-it "devrait reussir" do
-get :show, :id => @user
-expect(response).to have_http_status(:success)
-end
-
-it "devrait trouver le bon user" do
-get :show, :id => @user
-assigns(:user).to == @user
-end
-
-end
+		it "devrait trouver le bon user" do
+			get :show, params: {id: @user}
+			expect(assigns(:user)).to eq(@user)
+		end
+	end
 
   describe "GET #new" do
     it "returns http success" do
@@ -26,18 +26,10 @@ end
       expect(response).to have_http_status(:success)
     end
 
-  it "devrait avoir le bon titre" do
-	get :new
-	expect(response.body).to have_title('Inscription')
-  end 
-
+  	it "devrait avoir le bon titre" do
+			get :new
+			expect(response.body).to have_selector("title", text: "Inscription", visible: false)
+  	end 
   end
-
-
-
-
-
-
-
 
 end
