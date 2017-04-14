f8aa6539cc49968172f524bd6ce822607f5282d5
Thai Dang <thai.dang.fr@gmail.com>
2017-04-14 11:55:37 +0200

    fix spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,24 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-	@titre = @user.nom
+		@titre = @user.nom
   end
 
   def new
-	@user = User.new
-	@titre = "Inscription"
+		@user = User.new
+		@titre = "Inscription"
   end
 
   def create
-	@user = User.new(user_params)
-	if @user.save
-	flash[:success] = "Bienvenue dans l'application exemple !"
-	redirect_to @user
-	else
-	@titre = "Inscription"
-	render 'new'
-	end
+		@user = User.new(user_params)
+			if @user.save
+				sign_in @user
+				flash[:success] = "Bienvenue dans l'application exemple !"
+				redirect_to @user
+			else
+				@titre = "Inscription"
+				render 'new'
+			end
   end
 
 	private
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,12 +16,29 @@ RSpec.describe UsersController, type: :controller do
 
 		it "devrait trouver le bon user" do
 			get :show, params: {id: @user}
+			# assigns(:user) <=> @user
 			expect(assigns(:user)).to eq(@user)
 		end
+
+		it "devrait avoir le bon titre" do
+			get :show, params: {id: @user}
+			expect(response.body).to have_selector("title", text: @user.nom, visible: false)
+		end
+
+		it "devrait inclure le nom de l'utilisateur" do
+			get :show, params: {id: @user}
+			expect(response.body).to have_selector("h1", text: @user.nom)
+		end
+
+		it "devrait avoir une image de profil" do
+			get :show, params: {id: @user}
+			expect(response.body).to have_selector("h1>img", class: "gravatar")
+		end
+
 	end
 
   describe "GET #new" do
-    it "returns http success" do
+    it "devrait reussir" do
       get :new
       expect(response).to have_http_status(:success)
     end
@@ -31,5 +48,60 @@ RSpec.describe UsersController, type: :controller do
 			expect(response.body).to have_selector("title", text: "Inscription", visible: false)
   	end 
   end
+
+	describe "POST 'create'" do
+
+		describe "echec" do
+			before(:each) do
+				@attr = { :nom => "", :email => "", :password => "", :password_confirmation => "" }
+			end
+
+			it "ne devrait pas creer d'utilisateur" do
+				expect(lambda do
+					post :create, params: {user: @attr}
+				end).not_to change(User, :count)
+			end
+
+			it "devrait avoir le bon titre" do
+				post :create, params: {user: @attr}
+				expect(response.body).to have_selector("title", text: "Inscription", visible: false)
+			end
+
+			it "devrait rendre la page 'new'" do
+				post :create, params: {user: @attr}
+				expect(response).to render_template('new')
+			end
+		end
+
+		describe "succes" do
+			before(:each) do
+				@attr = { :nom => "New User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar" }
+			end
+
+			it "devrait creer un utilisateur" do
+				expect(lambda do
+					post :create, params: {user: @attr}
+				end).to change(User, :count).by(1)
+			end
+
+			it "devrait rediriger vers la page d'affichage de l'utilisateur" do
+				post :create, params: {user: @attr}
+				expect(response).to redirect_to(user_path(assigns(:user)))
+			end
+
+			it "devrait avoir un message de bienvenue" do
+				post :create, params: {user: @attr}
+				expect(flash[:success]).to match(/Bienvenue dans l'Application Exemple/i)
+			end
+
+			it "devrait identifier l'utilisateur" do
+				post :create, :user => @attr
+				expect(controller).to be_signed_in
+			end
+		end
+	end
+
+
+
 
 end
