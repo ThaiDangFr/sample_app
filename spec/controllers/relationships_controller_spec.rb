e0cc361fc1fbfaae370b9a1eb8d12f0622c254d8
Thai Dang <thai.dang.fr@gmail.com>
2017-04-26 23:14:43 +0200

    Un bouton de suivi standard

---
+++ spec/controllers/relationships_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe RelationshipsController do
+
+    describe "Le contrôle d'accès" do
+        it "devrait exiger l'identification pour créer" do
+            post :create
+            expect(response).to redirect_to(signin_path)
+        end
+
+        it "devrait exiger l'identification pour détruire" do
+            delete :destroy, params: {id:1}
+            expect(response).to redirect_to(signin_path)
+        end
+    end
+
+    describe "post create" do
+        before(:each) do
+            @user = test_sign_in(FactoryGirl.create(:user))
+            @followed = FactoryGirl.create(:user, :email => FactoryGirl.generate(:email))
+        end
+        
+        it "devrait créer une relation" do
+            expect(lambda do
+                post :create, params: {relationship: {:followed_id => @followed }}
+                expect(response).to be_redirect
+            end).to change(Relationship, :count).by(1)
+        end
+    end
+
+    describe "delete destroy" do
+        before(:each) do
+            @user = test_sign_in(FactoryGirl.create(:user))
+            @followed = FactoryGirl.create(:user, :email => FactoryGirl.generate(:email))
+            @user.follow!(@followed)
+            @relationship = @user.relationships.find_by_followed_id(@followed)
+        end
+
+        it "devrait détruire une relation" do
+            expect(lambda do
+                delete :destroy, params: {id:@relationship}
+                expect(response).to be_redirect
+            end).to change(Relationship, :count).by(-1)
+        end
+    end
+end
